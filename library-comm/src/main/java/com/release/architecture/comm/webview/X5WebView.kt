512b4a90bc4531ffc872e57f682df80c4b3d317f
延成 <cyc.release@gmail.com>
2021-12-29 15:11:53 +0800

    Initial commit

--- library-comm/src/main/java/com/release/architecture/comm/webview/X5WebView.kt
+++ library-comm/src/main/java/com/release/architecture/comm/webview/X5WebView.kt
@@ -4,12 +4,12 @@ import android.annotation.SuppressLint
 import android.content.Context
 import android.view.LayoutInflater
 import android.view.ViewGroup
-import com.tencent.smtt.sdk.WebChromeClient
-import com.tencent.smtt.sdk.WebView
-import com.tencent.smtt.sdk.WebViewClient
 import com.release.architecture.base.utils.logD
 import com.release.architecture.base.widget.StateLayout
 import com.release.architecture.comm.R
+import com.tencent.smtt.sdk.WebChromeClient
+import com.tencent.smtt.sdk.WebView
+import com.tencent.smtt.sdk.WebViewClient
 
 /**
  * 自定义x5WebView
@@ -44,8 +44,6 @@ class X5WebView(context: Context) : WebView(context) {
         initProgressBar()
         initLoadAni()
         initWebViewSettings()
-        this.webChromeClient = mChromeClient
-        this.view.isClickable = true
     }
 
     /**
@@ -62,7 +60,10 @@ class X5WebView(context: Context) : WebView(context) {
      * 初始化加载动画
      */
     private fun initLoadAni() {
-        mStateLayout = LayoutInflater.from(context).inflate(com.release.architecture.base.R.layout.base_include_status_layout, null) as StateLayout
+        mStateLayout = LayoutInflater.from(context).inflate(
+            com.release.architecture.base.R.layout.base_include_status_layout,
+            null
+        ) as StateLayout
         mStateLayout.layoutParams =
             LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT)
         mStateLayout.setBgColor(0x4D000000)
@@ -77,27 +78,28 @@ class X5WebView(context: Context) : WebView(context) {
     private fun initWebViewSettings() {
         // 关掉长按事件  避免跳出界面
         setOnLongClickListener { true }
-
         this.settings.apply {
-            javaScriptEnabled = true    // 允许js调用
-            javaScriptCanOpenWindowsAutomatically = true    //支持通过JS打开新窗口 弹窗
-            allowFileAccess = false // 禁止访问文件
-            setSupportZoom(false)   // 设置WebView是否支持使用屏幕控件或手势进行缩放，默认是true，支持缩放
-            builtInZoomControls = false // 设置WebView是否使用其内置的变焦机制，该机制集合屏幕缩放控件使用，默认是false
+            javaScriptEnabled = true //允许js调用
+            javaScriptCanOpenWindowsAutomatically = true //支持通过JS打开新窗口
+            databaseEnabled = true //设置可以调用数据库
+            domStorageEnabled = true //设置可以dom存储
+            loadsImagesAutomatically = true
+            allowFileAccess = false //在File域下，能够执行任意的JavaScript代码，同源策略跨域访问能够对私有目录文件进行访问等
+            setSupportZoom(false) //支持页面缩放
+            builtInZoomControls = false //进行控制缩放
             allowContentAccess = true //是否允许在WebView中访问内容URL（Content Url），默认允许
+            setGeolocationEnabled(false) //定位是否可用
             setSupportMultipleWindows(false) //设置WebView是否支持多窗口,如果为true需要实现onCreateWindow(WebView, boolean, boolean, Message)
-            setGeolocationEnabled(false) // 定位是否可用
-            loadWithOverviewMode = true // 是否允许WebView度超出以概览的方式载入页面，
-            setAllowUniversalAccessFromFileURLs(false) // 是否允许运行在一个file schema URL环境下的JavaScript访问来自其他任何来源的内容
-            setAllowFileAccessFromFileURLs(false) // 是否允许运行在一个URL环境（the context of a file scheme URL）中的JavaScript访问来自其他URL环境的内容，为了保证安全，应该不允许。
+            loadWithOverviewMode = true //是否允许WebView度超出以概览的方式载入页面，
+            defaultTextEncodingName = "GBK"
         }
         this.webViewClient = WebViewClient()
+        this.webChromeClient = mChromeClient
+        this.view.isClickable = true
     }
 
     override fun loadUrl(url: String) {
         logD(url, "WebViewUrl")
         super.loadUrl(url)
     }
-
-
 }
